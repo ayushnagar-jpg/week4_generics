2283a7d1bcddfc7231f744ff550f7e5d4e565cb6
Ayush Nagar <ayushnagar633@gmail.com>
2025-03-19 15:55:02 +0530

    [AYUSH]ADD:Multi-Level University Course Management System

---
+++ smarthouse/coursemanagmentsystm/UniversityUtility.java
@@ -0,0 +1,11 @@
+package smarthouse.coursemanagmentsystm;
+
+import java.util.List;
+
+public class UniversityUtility {
+    public static void displayAllCourses(List<? extends CourseType> courses) {
+        for (CourseType course : courses) {
+            System.out.println(course);
+        }
+    }
+}
