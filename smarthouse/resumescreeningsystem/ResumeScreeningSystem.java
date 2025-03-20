d8b0c920ae8c631a6e9d3f112592771f1e116942
Ayush Nagar <ayushnagar633@gmail.com>
2025-03-20 10:30:48 +0530

    [AYUSH]ADD:AI-Driven Resume Screening System

---
+++ smarthouse/resumescreeningsystem/ResumeScreeningSystem.java
@@ -0,0 +1,14 @@
+package smarthouse.resumescreeningsystem;
+
+
+import java.util.List;
+
+// AI Screening System
+public class ResumeScreeningSystem {
+    // Wildcard method to process multiple job roles
+    public static void processMultipleResumes(List<? extends JobRole> resumes) {
+        for (JobRole job : resumes) {
+            job.displayRoleDetails();
+        }
+    }
+}
