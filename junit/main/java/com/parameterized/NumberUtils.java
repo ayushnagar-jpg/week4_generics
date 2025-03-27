01a85ffb1dc7dade869020c35c0f2871a77274bf
Ayush Nagar <ayushnagar633@gmail.com>
2025-03-27 23:17:02 +0530

    [AYUSH]ADD: NumberUtils

---
+++ junit/main/java/com/parameterized/NumberUtils.java
@@ -0,0 +1,8 @@
+package com.parameterized;
+
+public class NumberUtils {
+    // ✅ Method to check if a number is even
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
