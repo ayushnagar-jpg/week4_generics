d5fabed15ba86bef696f70a7db290fc32d296606
Ayush Nagar <ayushnagar633@gmail.com>
2025-03-27 23:06:15 +0530

    [AYUSH]ADD: ExceptionHandling

---
+++ junit/main/java/com/listoperations/ListManager.java
@@ -0,0 +1,21 @@
+package com.listoperations;
+import com.sun.tools.javac.util.List;
+
+
+public class ListManager {
+         
+	public void addElement(java.util.List<Integer> list, int element) {
+		list.add(element);
+	}
+	
+	public boolean removeElement(java.util.List<Integer> list, int element) {
+	
+		return list.remove(Integer.valueOf(element));
+	}
+	
+	public int getSize(java.util.List<Integer> list) {
+		
+		return list.size();
+		
+	}
+}
